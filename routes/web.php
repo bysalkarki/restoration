5aae9585615e8fffd113574e9c24666e04750e5a
bishalkarki <bishalkarki201@gmail.com>
2021-11-13 08:24:48 +0545

    routes

--- routes/web.php
+++ routes/web.php
@@ -21,14 +21,14 @@ Route::prefix('admin')->middleware('auth')->group(function () {
     Route::get('/', function () {
         return view('admin.welcome');
     })->name('admin.index');
-    Route::resource('slider', App\Http\Controllers\SliderController::class);
-    Route::resource('video', App\Http\Controllers\VideoController::class);
-    Route::resource('blog', App\Http\Controllers\BlogController::class);
-    Route::resource('menu', App\Http\Controllers\MenuController::class);
-    Route::resource('category', App\Http\Controllers\CategoryController::class);
-    Route::resource('gallery', App\Http\Controllers\GalleryController::class);
-    Route::resource('event', App\Http\Controllers\EventController::class);
-    Route::resource('team', App\Http\Controllers\TeamController::class);
-    Route::resource('partner', App\Http\Controllers\PartnerController::class);
+    Route::resource('slider', App\Http\Controllers\SliderController::class)->except('show');
+    Route::resource('video', App\Http\Controllers\VideoController::class)->except('show');
+    Route::resource('blog', App\Http\Controllers\BlogController::class)->except('show');
+    Route::resource('menu', App\Http\Controllers\MenuController::class)->except('show');
+    Route::resource('category', App\Http\Controllers\CategoryController::class)->except('show');
+    Route::resource('gallery', App\Http\Controllers\GalleryController::class)->except('show');
+    Route::resource('event', App\Http\Controllers\EventController::class)->except('show');
+    Route::resource('team', App\Http\Controllers\TeamController::class)->except('show');
+    Route::resource('partner', App\Http\Controllers\PartnerController::class)->except('show');
     Route::resource('appsetting', App\Http\Controllers\AppsettingController::class)->only('index', 'store');
 });
